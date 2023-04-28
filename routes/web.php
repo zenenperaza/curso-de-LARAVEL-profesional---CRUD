0b9e79837361a5dc9a66aeb1a6751c8c4e4586a4
zenenperaza <peraza@outlook.com>
2023-04-28 13:35:52 -0400

    commits

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/product/{id}',[Controller::class, 'function'])->name('example');
+Route::get('/product/{id}',[NoteController::class, 'index'])->name('note.index');
